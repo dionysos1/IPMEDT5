1d58b003913bfa558b181d104e6a2a04a5383f93
Dion van den Berg <dionberg@gmail.com>
2018-04-13 13:43:37 +0200

    Final version with last minute fixes and cleanup

--- WorkerScript.py
+++ WorkerScript.py
@@ -1,11 +1,11 @@
-import threading
-from time import sleep
 import time
 import serial
 import MySQLdb
-import sys
 
-# from timeit import default_timer as timer
+# initialiseer de usb devices
+# ACM0 is een arduino UNO met de gewichtssensor en RFID lezer.
+# USB0 is een arduino NANO met RFID lezer.
+# USB1 is een arduino NANO met een LCD scherm eraan.
 
 usb_ports = [
     serial.Serial('/dev/ttyACM0', 9600),
@@ -13,15 +13,9 @@ usb_ports = [
     serial.Serial('/dev/ttyUSB1', 9600)
 ]
 
-display_text = 'test;test2'
-
+# initialiseer de connectie met de database
 db = MySQLdb.connect("localhost", "root", "fractal", "zelfservice")
 db_cursor = db.cursor()
-
-KG_string = 'kg'  # check string om te kijken of er gewicht wordt doorgestuurd
-UID_string = 'UID'  # check string om te kijken of er een UID wordt doorgestuurd
-
-active = True
 
 
 def serial_read_product(usb):
@@ -31,26 +25,27 @@ def serial_read_product(usb):
 
         split = incoming_string.split(' ')
 
-        uid_and_weight_to_db(int(split[0]), float(split[1]))
+        uid_and_weight_to_db(str(split[0]), float(split[1]))
 
 
 def serial_read_product_id(read_serial, write_serial):
     # lees de data van de seriele verbinding
     if read_serial.in_waiting:
         uid = read_serial.readline()
-
+        # vraag aan db wat deze tag is en kost.
         product = get_product_by_rfid(uid[:8])
         if not product:
             serial_write(write_serial, uid + "Ready...;Scan een product")
         if product:
             serial_write(write_serial, str(product[0]) + ";" + str(product[2]))
-            # serial_write(write_serial, str(product[0]) + ";test")
-
 
 
 def get_product_by_rfid(rfid):
+    # vraag aan de db wat de naam en kosten zijn van de opgegeven tag.
     db_cursor.execute("SELECT name, rfid, price FROM product_status WHERE rfid = '{}'".format(rfid))
+
     results = db_cursor.fetchall()
+    # als het product niet bekend is geef dan aan dat het product onbekend is.
     if len(results) == 0:
         return "UNKNOWN PRODUCT", "UNKNOWN PRODUCT", "UNKNOWN PRODUCT"
     else:
@@ -65,28 +60,9 @@ def serial_write(usb, args):
 
 def uid_and_weight_to_db(uid, weight):
     # schrijf het huidige gewicht en uid naar de DB
-    try:
-        print("schrijf gewicht en uid naar database...")
-        sys.stdout.flush()
-
-        db_cursor.execute("UPDATE gebruiker_dump SET rfid = '{}', kg = '{}' WHERE id = 1".format(uid, weight))
-        db.commit()
-    except:
-        print "Error: the database is being rolled back"
-        db.rollback()
-
-
-def product_read(usb):
-    while True:
-
-        serial_read_product(usb)
-        time.sleep(.1)
-
-
-def product_read_push_screen(read_usb, screen_usb):
-    while True:
-        serial_read_product_id(read_usb, screen_usb)
-        time.sleep(.1)
+    db_cursor.execute("UPDATE gebruiker_dump SET rfid = '{}', kg = '{}' WHERE id = 1".format(uid, weight))
+    time.sleep(.1)
+    db.commit()
 
 
 def main():
@@ -94,18 +70,10 @@ def main():
     for usb in usb_ports:
         usb.flushInput()
 
-    threads = [
-        threading.Thread(target=product_read, args=(usb_ports[0])),
-        threading.Thread(target=product_read_push_screen, args=(usb_ports[1], usb_ports[2])),
-    ]
-
-    print(get_product_by_rfid("0415bba2"))
-
-    for thread in threads:
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+    while True:
+        serial_read_product(usb_ports[0])
+        serial_read_product_id(usb_ports[1], usb_ports[2])
+        time.sleep(.1)
 
 
 if __name__ == '__main__':
